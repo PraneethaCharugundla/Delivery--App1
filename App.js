ff70fee1579487e1c4362bf5822ee081b512ddd8
Praneetha Charugundla <88439139+PraneethaCharugundla@users.noreply.github.com>
2023-05-02 15:21:55 +0530

    Docs - changes in documentation

--- App.js
+++ App.js
@@ -55,46 +55,612 @@ const Header = () => {
 
 const restauratList =[
     {
-        id: "1",
-        name: " Grand Hotel",
-        image: "g1arzw6qd7g9affkmgzk",
-        cuisines: ["Chinese", "Biriyani", "Haleem"],
-        rating: "4.0"
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "703934",
+          "name": "Sagar Papaji Ka Dhaba ( Since 1985)",
+          "uuid": "0d436029-ae8f-41b1-bec0-8859750df49d",
+          "city": "3",
+          "area": "Hanuman tekdi- Abids",
+          "totalRatingsString": "Too Few Ratings",
+          "cloudinaryImageId": "f34657e4f398eec0e714971b58f8be69",
+          "cuisines": [
+            "North Indian",
+            "Punjabi",
+            "Chinese",
+            "Beverages"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 85000,
+          "costForTwoString": "₹850 FOR TWO",
+          "deliveryTime": 34,
+          "minDeliveryTime": 34,
+          "maxDeliveryTime": 34,
+          "slaString": "34 MINS",
+          "lastMileTravel": 1.2000000476837158,
+          "slugs": {
+            "restaurant": "sagar-papaji-ka-dhaba-himayath-nagar-himayath-nagar",
+            "city": "hyderabad"
+          },
+          "cityState": "3",
+          "address": "4-1-365/F TO F4,E1 TO E3,D1 TO D3, G&F FLOORS,PAPAJI ENCLAVE,REDDY HOTEL ROAD,BOGGULKUNTA,HYDERABAD,TELANGANA",
+          "locality": "Papaji Enclave",
+          "parentId": 174498,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "aggregatedDiscountInfo": {
+            "header": "20% off",
+            "shortDescriptionList": [
+              {
+                "meta": "20% off | Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "20% off up to ₹50 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "aggregatedDiscountInfoV2": {
+            "header": "20% OFF",
+            "shortDescriptionList": [
+              {
+                "meta": "Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "20% off up to ₹50 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "ribbon": [
+            {
+              "type": "PROMOTED"
+            }
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 2900,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 2900,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "2900",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "cid=6563784~p=1~eid=00000187-db9e-fe0b-1d59-82c100e40106",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "1.2 kms",
+          "hasSurge": false,
+          "sla": {
+            "restaurantId": "703934",
+            "deliveryTime": 34,
+            "minDeliveryTime": 34,
+            "maxDeliveryTime": 34,
+            "lastMileTravel": 1.2000000476837158,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": true,
+          "avgRating": "--",
+          "totalRatings": 0,
+          "new": true
+        },
+        "subtype": "basic"
       },
       {
-        id: "2",
-        name: "Santosh Family Dabha",
-        image: "kxvudr9703tklnjsolsv",
-        cuisines: ["North India", "Chinese", "Tandoor"],
-        rating: "4.0"
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "4320",
+          "name": "Mehfil",
+          "uuid": "4dee4970-8673-42b6-8a49-cab0efdb258f",
+          "city": "3",
+          "area": "Narayanguda",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "esp0fhddgfgdss5hyhpi",
+          "cuisines": [
+            "Biryani",
+            "North Indian",
+            "Tandoor",
+            "Chinese",
+            "Kebabs"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 30000,
+          "costForTwoString": "₹300 FOR TWO",
+          "deliveryTime": 29,
+          "minDeliveryTime": 29,
+          "maxDeliveryTime": 29,
+          "slaString": "29 MINS",
+          "lastMileTravel": 2,
+          "slugs": {
+            "restaurant": "mehfil-narayanguda-himayath-nagar",
+            "city": "hyderabad"
+          },
+          "cityState": "3",
+          "address": "Near Narayanguda Flyover, Narayanguda, Hyderabad",
+          "locality": "Near Narayanguda Flyover",
+          "parentId": 637,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "aggregatedDiscountInfo": {
+            "header": "10% off",
+            "shortDescriptionList": [
+              {
+                "meta": "10% off | Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "10% off up to ₹40 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "aggregatedDiscountInfoV2": {
+            "header": "10% OFF",
+            "shortDescriptionList": [
+              {
+                "meta": "Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "10% off up to ₹40 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 2900,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 2900,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "2900",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "2 kms",
+          "hasSurge": false,
+          "sla": {
+            "restaurantId": "4320",
+            "deliveryTime": 29,
+            "minDeliveryTime": 29,
+            "maxDeliveryTime": 29,
+            "lastMileTravel": 2,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": false,
+          "avgRating": "3.9",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
       },
       {
-        id: "3",
-        name: "Lucky restaurant ",
-        image: "qems028i3da5rxf81rbp",
-        cuisines: ["Biriyani", "North India", "Tandoor"],
-        rating: "3.8"
-      }
-
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "17345",
+          "name": "Papadams Blue",
+          "uuid": "722eafdf-33c6-4f5b-9139-5282b18df81e",
+          "city": "3",
+          "area": "Huda Complex",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "ew03odqbnyxenhiqmjeh",
+          "cuisines": [
+            "Biryani",
+            "Chinese",
+            "Andhra",
+            "North Indian",
+            "South Indian",
+            "Tandoor"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 50000,
+          "costForTwoString": "₹500 FOR TWO",
+          "deliveryTime": 40,
+          "minDeliveryTime": 40,
+          "maxDeliveryTime": 40,
+          "slaString": "40 MINS",
+          "lastMileTravel": 6.5,
+          "slugs": {
+            "restaurant": "papadams-blue-near-pvt-market-kothapet-kothapet-dilsukhnagar",
+            "city": "hyderabad"
+          },
+          "cityState": "3",
+          "address": "Lane Opposite Omni Hospital, Kothapet Cross Roads, Hyderabad",
+          "locality": "Kothapet Cross Road",
+          "parentId": 3343,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "aggregatedDiscountInfo": {
+            "header": "30% off",
+            "shortDescriptionList": [
+              {
+                "meta": "30% off | Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "30% off up to ₹75 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "aggregatedDiscountInfoV2": {
+            "header": "30% OFF",
+            "shortDescriptionList": [
+              {
+                "meta": "Use TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "descriptionList": [
+              {
+                "meta": "30% off up to ₹75 | Use code TRYNEW",
+                "discountType": "Percentage",
+                "operationType": "RESTAURANT"
+              }
+            ],
+            "subHeader": "",
+            "headerType": 0,
+            "superFreedel": ""
+          },
+          "ribbon": [
+            {
+              "type": "PROMOTED"
+            }
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 5300,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 5300,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "5300",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "cid=6630403~p=4~eid=00000187-db9e-fe0b-1d59-82c200e4047d",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "6.5 kms",
+          "hasSurge": false,
+          "sla": {
+            "restaurantId": "17345",
+            "deliveryTime": 40,
+            "minDeliveryTime": 40,
+            "maxDeliveryTime": 40,
+            "lastMileTravel": 6.5,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": true,
+          "avgRating": "4.0",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
+      {
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "4650",
+          "name": "Bawarchi",
+          "uuid": "4e784916-87ff-4951-9c21-2f0c1df1fb31",
+          "city": "3",
+          "area": "New Nallakunta",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "smfhai0o8rnjuzp3ngxu",
+          "cuisines": [
+            "Biryani",
+            "Tandoor",
+            "Chinese",
+            "Indian",
+            "Desserts",
+            "Kebabs",
+            "Mughlai"
+          ],
+          "tags": [
+            
+          ],
+          "costForTwo": 30000,
+          "costForTwoString": "₹300 FOR TWO",
+          "deliveryTime": 41,
+          "minDeliveryTime": 41,
+          "maxDeliveryTime": 41,
+          "slaString": "41 MINS",
+          "lastMileTravel": 3,
+          "slugs": {
+            "restaurant": "bawarchi-nallakunta-vidyanagar",
+            "city": "hyderabad"
+          },
+          "cityState": "3",
+          "address": "Beside Astoria Hotel, RTC Cross Road, Chikkadpally, Nallakunta, Hyderabad",
+          "locality": "RTC X roads",
+          "parentId": 100,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [
+            
+          ],
+          "chain": [
+            
+          ],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 3700,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 3700,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "3700",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "",
+          "badges": {
+            "imageBased": [
+              
+            ],
+            "textBased": [
+              
+            ],
+            "textExtendedBadges": [
+              
+            ]
+          },
+          "lastMileTravelString": "3 kms",
+          "hasSurge": false,
+          "sla": {
+            "restaurantId": "4650",
+            "deliveryTime": 41,
+            "minDeliveryTime": 41,
+            "maxDeliveryTime": 41,
+            "lastMileTravel": 3,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": false,
+          "avgRating": "4.0",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
 ] 
 const RestaurantCard = () => {
     return (
         <div className="card">
-            <img src={burgerKing.image}/>
-            <h2>{burgerKing.name} </h2>
-            <h3>{burgerKing.cusines.join(" , ")}</h3>
-            <h4>{burgerKing.rating} stars </h4>
+            <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+        restauratList[1].data?.cloudinaryImageId 
+        } 
+        />
+            <h2>{restauratList[0].data?.name} </h2>
+            <h3>{restauratList[0].data?.cuisines.join(" , ")}</h3>
+            <h4>{restauratList[0].data?.lastMileTravel} minutes  </h4>
         </div>
     );
 };
 
 const Body = () => {
     return(
-        <div>
+        <div className="restaurant-list">
 
-            <RestaurantCard/>
-            <RestaurantCard/>
-            <RestaurantCard/>
+            <RestaurantCard  restaurant = {restauratList[0]}/>
+            <RestaurantCard restaurant = {restauratList[1]} />
+            <RestaurantCard  restaurant = {restauratList[2]}/>
+            <RestaurantCard  restaurant = {restauratList[3]}/>
         </div>
     )
 }
@@ -116,4 +682,5 @@ const AppLayout = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>);
+
+root.render(<RestaurantCard/>);
